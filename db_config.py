5d7b28b6deefac5d30a88922bf285e49466701c8
Apurv Gupta <186622906+Apurv7Gupta@users.noreply.github.com>
2026-03-04 07:24:37 +0530

    Create db_config.py

---
+++ db_config.py
@@ -0,0 +1,45 @@
+import os
+from langchain_huggingface import HuggingFaceEndpointEmbeddings
+from langchain_pinecone import PineconeVectorStore
+from pinecone import Pinecone
+
+
+from dotenv import load_dotenv
+
+load_dotenv()
+
+# --- EMBEDDINGS + CLIENT + VectorDB---
+PINECONE_API_KEY = os.getenv("PINECONE_API_KEY")
+INDEX_NAME = os.getenv("PINECONE_INDEX_NAME", "collection1")
+
+
+def get_vector_db() -> PineconeVectorStore:
+    embeddings = HuggingFaceEndpointEmbeddings(
+        model="sentence-transformers/all-MiniLM-L6-v2",
+        huggingfacehub_api_token=os.getenv("HF_TOKEN"),
+    )
+    pc = Pinecone(api_key=PINECONE_API_KEY)
+    index = pc.Index(INDEX_NAME)
+
+    return PineconeVectorStore(index=index, embedding=embeddings)
+
+
+# -------------To Delete DB----------------------------
+pc = Pinecone(api_key=PINECONE_API_KEY)
+index = pc.Index(INDEX_NAME)
+
+# Delete by ID
+# index.delete(ids=["id1", "id2"])
+
+# Delete by metadata filter
+# index.delete(filter={"source": "company_report.md"})
+
+# Delete ALL
+# index.delete(delete_all=True)
+# -----------------------------------------------------
+
+
+# embeddings = HuggingFaceEmbeddings(model_name="sentence-transformers/all-MiniLM-L6-v2") // not using Local embedding gen
+# vector_db = Chroma(persist_directory="./chroma_db", embedding_function=embeddings)    // not using local persistence
+
+# Will probably be swapped with Qdrant Cloud (free tier)
